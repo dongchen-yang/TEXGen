34facc007fd4a1ff5fca534c07842d4a10bd7000
dya78 <dya78@cs-3dlg-23.cmpt.sfu.ca>
2026-02-02 23:29:57 -0800

    update safety check

--- launch.py
+++ launch.py
@@ -8,6 +8,16 @@ import sys
 os.environ['HF_HUB_DISABLE_EXPERIMENTAL_WARNING'] = '1'
 os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
+# Fix for PyTorch 2.6 checkpoint loading with OmegaConf
+# PyTorch 2.6 changed weights_only default to True, but checkpoints contain OmegaConf objects
+import torch.serialization
+try:
+    from omegaconf import DictConfig, ListConfig
+    from omegaconf.base import ContainerMetadata
+    torch.serialization.add_safe_globals([ContainerMetadata, DictConfig, ListConfig])
+except ImportError:
+    pass  # OmegaConf not yet imported, will be handled later
+
 
 class ColoredFilter(logging.Filter):
     """
